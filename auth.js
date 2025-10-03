203f46a7c881c1d824ea3892d6691904b4f6e5d6
YeoleKrushna <yeolekrushna00@gmail.com>
2025-10-04 02:40:18 +0530

    Full Frontend

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'http://72.60.202.62';
 
 function showError(message) {
     const errorDiv = document.getElementById('error-message');
